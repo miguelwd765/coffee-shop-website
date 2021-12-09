16ebb245a6ad9bcf23df4714ca5077e818b9953e
Miguel Pacheco <miguelwebdev765@gmail.com>
2021-12-09 13:18:39 -0600

    add functionality to close dropdown when clicked outside of it

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const shopBtn = document.querySelector("#shop");
 const menuContent = document.querySelector(".menu-content");
+const dropDown = document.querySelector(".dropdown-menu");
 
 shopBtn.addEventListener("click", function (e) {
   menuContent.classList.toggle("menu-content-show");
+
+  window.addEventListener("click", function (e) {
+    if (
+      e.target.matches(".links-hidden") ||
+      e.target.matches(".dropdown-menu") ||
+      e.target.matches(".menu-btn") ||
+      e.target.matches(".items-hidden") ||
+      e.target.matches("#drop-icon")
+    ) {
+      return;
+    } else {
+      menuContent.classList.remove("menu-content-show");
+    }
+  });
 });
