06d5d6bcc9e40e3f42fbf52a738586f2b06f80c8
Miguel Pacheco <miguelwebdev765@gmail.com>
2021-12-10 09:09:23 -0600

    make nav mobile responsive. Continue work on shop dropdown button to make functional

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const shopBtn = document.querySelector("#shop");
+const shopBtn = document.querySelector(".menu-btn");
 const menuContent = document.querySelector(".menu-content");
 const dropDown = document.querySelector(".dropdown-menu");
 
@@ -17,4 +17,29 @@ shopBtn.addEventListener("click", function (e) {
       menuContent.classList.remove("menu-content-show");
     }
   });
+});
+
+const navToggle = document.querySelector(".nav-toggle");
+const navMobile = document.querySelector(".nav-mobile");
+const navMobileLinks = document.querySelectorAll(".nav-mobile-link");
+
+navToggle.addEventListener("click", function () {
+  navMobile.classList.toggle("nav-mobile-active");
+
+  navMobileLinks.forEach((links, index) => {
+    if (links.style.animation) {
+      links.style.animation = "";
+    } else {
+      links.style.animation = `navLinksFade .1s ease forwards ${
+        index / 10 + 0.1
+      }s`;
+    }
+  });
+});
+
+const mobileMenuContent = document.querySelector(".mobile-menu-content");
+const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
+
+mobileMenuBtn.addEventListener("click", function (e) {
+  mobileMenuContent.classList.toggle("menu-content-show");
 });
